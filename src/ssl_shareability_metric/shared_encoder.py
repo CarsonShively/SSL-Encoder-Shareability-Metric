f32e7824a4194ddfc98751fc95c2660471b4964c
Carson Shively <carsonshively24@gmail.com>
2026-08-21 05:15:37 -0700

    backup

--- src/ssl_shareability_metric/shared_encoder.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-class SharedEncoder(torch.nn.Module):
-    def __init__(self, vector_size):
-        super().__init__()
-        self.shared = torch.nn.Linear(vector_size, 1, bias=False)
-        
-    def forward(self, x):
-        return self.shared(x)
